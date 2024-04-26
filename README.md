defac7a88d2eda24b4f6890326e5e9c1a5a8d504
learnwithfair <learnwithfair@gmail.com>
2024-04-26 13:11:43 +0600

    Chat App with mern

---
+++ README.md
@@ -0,0 +1,9 @@
+## Run the client server
+
+cd client
+npm start
+
+## Run Server (Command)
+
+In the current directory
+node server.js
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const io = require("socket.io")(8080, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+// app Use
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+
+    res.send("Welcome");
+})
+
+
+
+let users = []; // for Data store
+io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
+    
+    socket.on("chat", (data) => {       
+        users.push(data);
+        io.emit("transfer", data);
+
+    });
+
+    socket.on("disconnect", function () {
+        console.log("Disconnect");
+
+    })
+
+
+});
+
+app.listen(8000, () => {
+    console.log("Server Run at http://localhost:8080");
+})
